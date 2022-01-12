dee419040b1497b2944ae964034d75c4eaf42f8a
stuartrucker <stuart.a.rucker@gmail.com>
2022-01-12 17:34:31 -0500

    ee

--- scraping_functions/twitter_ens.py
+++ scraping_functions/twitter_ens.py
@@ -34,7 +34,7 @@ def get_following_raw(twitter_id, pagination_token=None):
   return json_response
 
 def extract_ens(s):
-    m = re.search('[\w.]+.eth',s)
+    m = re.search('[\w.]+\.eth',s)
     return m.group(0) if m else None
 
  
